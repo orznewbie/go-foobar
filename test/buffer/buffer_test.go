bc5b9a56cab73a25a792d2d6f74ca06e0904c99f
huhaolong <huhaolong@xbrother.com>
2022-04-25 16:27:00 +0800

    add buffer and sql test

---
+++ test/buffer/buffer_test.go
@@ -0,0 +1,14 @@
+package buffer
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+)
+
+func TestBytesBuffer(t *testing.T) {
+	var buf bytes.Buffer
+	buf.Write([]byte("Hello "))
+	buf.WriteString("World!")
+	fmt.Println(buf.String())
+}
---
+++ test/sql/mysql_test.go
@@ -0,0 +1,72 @@
+package sql
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/orznewbie/gotmpl/pkg/log"
+	"testing"
+)
+
+func mysqlDB() *sql.DB {
+	DB, err := sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/test")
+	if err != nil {
+		panic(err)
+	}
+	DB.SetConnMaxLifetime(100)
+	DB.SetMaxIdleConns(10)
+	return DB
+}
+
+func TestConn(t *testing.T) {
+	DB := mysqlDB()
+	//验证连接
+	if err := DB.Ping(); err != nil {
+		t.Fatal(err)
+	}
+	log.Info("connect to mysql successfully.")
+}
+
+type User struct {
+	Name string `json:"name"`
+	Age  int32  `json:"age"`
+}
+
+func TestQuery(t *testing.T) {
+	DB := mysqlDB()
+
+	var user User
+	rows, err := DB.Query("SELECT * FROM user")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		if err := rows.Scan(&user.Name, &user.Age); err != nil {
+			log.Error(err)
+		}
+		log.Info(user)
+	}
+}
+
+func TestAdd(t *testing.T) {
+	DB := mysqlDB()
+
+	tx, err := DB.Begin()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	stmt, err := tx.Prepare("INSERT INTO user (`name`, `age`) VALUES (?, ?)")
+	if err != nil {
+		t.Fatal(err)
+	}
+	res, err := stmt.Exec("李四", 30)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tx.Commit()
+
+	log.Info(res.LastInsertId())
+}
